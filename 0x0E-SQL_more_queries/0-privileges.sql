90007d808e7bd900c0117b8129fc070de993d1e3
fistum1 <fistumitiku@gmail.com>
2023-08-16 07:37:44 -0700

    SQL more queries

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,3 +1,5 @@
 -- It lists all privileges of the MySQL users
-SHOW GRANTS FOR user_0d_1@localhost AND user_0d_2@localhost;
+-- For user_0d_1 and user_od_2 on my server
+SHOW GRANTS FOR user_0d_1@localhost;
+SHOW GRANTS FOR user_0d_2@localhost;
 
--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,7 +1,4 @@
--- It creates a user and password
-CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost';
-ALTER USER 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
-GRANT ALL PRIVILEGES ON * . * TO 'user_0d_1'@localhost WITH GRANT OPTION;
-FLUSH PRIVILEGES;
- 
+-- It creates a user and password for user_0d_1
+CREATE USER IF NOT EXISTS user_0d_1@localhost IDENTIFIED BY 'user_0d_1_pwd';
+GRANT ALL PRIVILEGES ON * . * TO user_0d_1@localhost; 
 

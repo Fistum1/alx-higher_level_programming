39c6776c3484d9c5706bbe5f9f39e969eb4da806
fistum1 <fistumitiku@gmail.com>
2023-09-13 14:37:36 +0300

    JS objects scopes closures

--- 0x13-javascript_objects_scopes_closures/0-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/0-rectangle.js
@@ -2,4 +2,3 @@
 // Create empty class Rectangle that defines a rectangle
 class Rectangle {}
 module.exports = Rectangle;
-
--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -8,6 +8,4 @@ class Rectangle {
     }
   }
 }
-
 module.exports = Rectangle;
-

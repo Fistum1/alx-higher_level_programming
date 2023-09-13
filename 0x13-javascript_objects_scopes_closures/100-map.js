26db4974bc739684d2ab54bcb7237b70d9fe36d3
fistum1 <fistumitiku@gmail.com>
2023-09-13 15:06:35 +0300

    JS objects scopes closures

---
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+// Create a script that imports an array and computes a new array.
+const originalList = require('./100-data').list;
+console.log(originalList);
+const mappedList = originalList.map(function (e, index) {
+  return (e * index);
+});
+console.log(mappedList);
---
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+// Create a function that returns the number of occurrences in a list
+const originalList = require('./100-data').list;
+console.log(originalList);
+const mappedList = originalList.map(function (e, index) {
+  return (e * index);
+});
+console.log(mappedList);

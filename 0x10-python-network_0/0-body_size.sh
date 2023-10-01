b50ea870bd793eb3653a5d1efca541c780a68f6a
Fistum1 <fistumitiku1@gmail.com>
2023-10-01 16:33:07 +0300

    python network 0

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # It takes in a URL, sends a request and displays the size of the body
-curl - sI  ALLOW $1 - L | grep "Allow" | cut - d " " - f2-
+curl -sI "$1" | grep "Content-Length" | cut -d " " -f2
